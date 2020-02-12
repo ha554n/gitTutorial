e1e88ddbaa6b83256cfe1ed43abbf75f5317f4cf
Hassan Aleem <ha438@georgetown.edu>
2020-02-12 00:38:40 -0500

    added imports

--- testfunc.py
+++ testfunc.py
@@ -1,3 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import os
+from math import pi
+from itertools import combinations_with_replacement
+import pickle
+from IPython.display import display
+# pd.set_option('display.max_rows', 500)
+# pd.options.display.max_colwidth=100
+from natsort import natsorted
+import seaborn as sns
+import scipy.stats as stats 
+from collections import Counter
+from itertools import groupby
+from operator import itemgetter
+from itertools import combinations
+import time
+import math as math
+import inspect
+
+
+
+
+
+
 def atoi(text):
     '''
     returns int parts of strings
@@ -11,13 +38,3 @@ def natural_keys(text):
     (See Toothy's implementation in the comments)
     '''
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
-
-def foo(n):
-    return(n*3)
-
-class bar():
-    def __init__(self,name):
-
-        self.name=name
-
-        
