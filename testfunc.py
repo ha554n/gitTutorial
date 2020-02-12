0fef35edc14032b9c3daa121f5fef4de5d7075d8
Hassan Aleem <ha438@georgetown.edu>
2020-02-12 00:10:02 -0500

    added ati,natkeys

--- testfunc.py
+++ testfunc.py
@@ -1,9 +1,13 @@
-def foo(n):
-    return(n*3)
+def atoi(text):
+    '''
+    returns int parts of strings
+    '''
+    return int(text) if text.isdigit() else text
 
-class bar():
-    def __init__(self,name):
-
-        self.name=name
-
-        
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
